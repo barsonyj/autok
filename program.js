0335ef587a61f90f2d437dfa0bf84f3f89bfa218
Bársony József <barsony@kkando.hu>
2026-01-30 11:05:04 +0100

    Update program.js

--- program.js
+++ program.js
@@ -2,7 +2,8 @@ import { legjobb } from "./autok.js";
 
 console.log("Program elindult");
 
-for (let i=1; i<=5; i++) console.log(i, "autó");
+for (let i=1; i<=7; i++) console.log(i, "autó");
+legjobb();
 legjobb();
 
 console.log("Kész.")
